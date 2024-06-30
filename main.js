e64a0bf5c05dc20086f7aa3a1506f5da0b239b53
rubaisha2006 <rubaishaaltaf@outlook.com>
2024-06-30 11:37:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Unary operators {slide No 117 } 
+// (++), (--) {prefix and postfix operators}
+var index = 10;
+var result = ++index + ++index + 6 + --index - 2;
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Unary operators {slide No 117 } 
+// (++), (--) {prefix and postfix operators}
+// let index=10
+        //10+1=11 +//11+1=12+//23+6=29 +//12-1=11
+            //11+12=23              //29+11=40 //40-2=38 
+// let result=(++index) + (++index)   + 6 +   (--index)   -2
+// console.log(result);
